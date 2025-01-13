19735f6daa739b1c4694cae17b44ff9548de0f51
silverhyuk <tldn233@gmail.com>
2025-01-13 22:48:00 +0900

    변수명 result 치환

--- src/main/java/com/david/chapter01/Statement.java
+++ src/main/java/com/david/chapter01/Statement.java
@@ -39,28 +39,28 @@ public class Statement {
     }
 
     private double amountFor(Invoice.Performance perf, Play play) {
-        double thisAmount = 0;
+        double result = 0;
 
         switch (play.getType()) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.getAudience() > 30) {
-                    thisAmount += 1000 * (perf.getAudience() - 30);
+                    result += 1000 * (perf.getAudience() - 30);
                 }
                 break;
 
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.getAudience() > 20) {
-                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                    result += 10000 + 500 * (perf.getAudience() - 20);
                 }
-                thisAmount += 300 * perf.getAudience();
+                result += 300 * perf.getAudience();
                 break;
 
             default:
                 throw new IllegalArgumentException("알 수 없는 장르: " + play.getType());
         }
-        return thisAmount;
+        return result;
     }
 }
 
